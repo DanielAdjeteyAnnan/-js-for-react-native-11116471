48c26c823c9070b5f9b4cb2753142a91521638bc
AmgMoni-Fx <danieladjeteyannan@gmail.com>
2024-05-19 17:24:58 +0000

    Task 1 has been merged to task 2  ( arryManipulation.js)

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,3 +1,4 @@
+                 //Task 1
          function processArray(arr) {
             return arr.map(num => {
               if (num % 2 === 0) {
@@ -11,4 +12,28 @@
           let inputArray = [5, 6, 7, 8, 9 ,10];
         let resultArray = processArray(inputArray);
         console.log(resultArray)
+
+                 //Task 2
+        
+  function formatArrayStrings(strings, numbers) {
+    return strings.map((string, index) => {
+      const processedNumber = numbers[index];
+      if (processedNumber % 2 === 0) {
+        return string.toUpperCase();  
+      } else {
+        return string.toLowerCase();  
+      }
+    });
+  }
+  
+
+  const strings = ['elantra', 'car', 'year', 'date', 'energy','money'];
+  const numbers = [5, 6, 7, 8, 9, 10];
+  
+  const processedNumbers = processArray(numbers);
+  console.log(processedNumbers);
+  
+  const formattedStrings = formatArrayStrings(strings, processedNumbers);
+  console.log(formattedStrings);
+
         
